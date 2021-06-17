f6556dd055cbf72e52df7693f4c719c5996f35c2
Batuhan <59096077+uberfresh@users.noreply.github.com>
2021-06-18 00:44:21 +0300

    Update settings.py

--- imagedetection/imagedetection/settings.py
+++ imagedetection/imagedetection/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y%%-tm0=r(fi7^^6renn73u)07*g8t#t5l9y580z%9=6b1s83e'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
